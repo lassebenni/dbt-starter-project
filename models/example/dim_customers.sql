668ad9eead7d70d9698a5b9205c11f039b5ea2e3
Lasse Benninga <lassebenninga@godatadriven.com>
2022-02-02 15:28:29 +0000

    update2

--- models/example/dim_customers.sql
+++ models/example/dim_customers.sql
@@ -1,24 +1,13 @@
+{{ config (
+    materialized="table"
+)}}
+
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from jaffle_shop_customers
-
+    select * from {{ ref('stg_customers')}}
 ),
 
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from jaffle_shop_orders_csv
-
+    select * from {{ ref('stg_orders')}}
 ),
 
 customer_orders as (
---
+++ models/example/stg_orders.sql
@@ -0,0 +1,12 @@
+with orders as (
+    
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from jaffle_shop_orders_csv
+)
+
+select * from orders
