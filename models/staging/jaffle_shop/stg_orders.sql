0f7ab4b3862f34a6e0e0637a74a7689aa8bd378e
Lasse Benninga <lassebenninga@godatadriven.com>
2022-02-03 14:43:45 +0100

    reformat sql

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,12 +1,9 @@
 with orders as (
-    
-    select
-        id as order_id,
+    select id as order_id,
         user_id as customer_id,
         order_date,
         status
-
     from jaffle_shop_orders_csv
 )
-
-select * from orders
+select *
+from orders
