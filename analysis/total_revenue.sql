45db5084aeba510c16b7a6774dcec3e15bdcbee9
Lasse Benninga <lassebenninga@godatadriven.com>
2022-02-09 11:59:06 +0100

    Add total revenue analysis

---
+++ analysis/total_revenue.sql
@@ -0,0 +1,18 @@
+WITH payments AS (
+    SELECT
+        *
+    FROM
+        {{ ref('stg_payments') }}
+),
+aggregated AS (
+    SELECT
+        SUM(amount) AS total_revenue
+    FROM
+        payments
+    WHERE
+        status = 'success'
+)
+SELECT
+    *
+FROM
+    aggregated
